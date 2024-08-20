dc206d4c4d72aa2f71d38fae5ce5ca61d68c5fdf
Shubham Sharma <shubham.sharma@fplabs.tech.in>
2024-08-20 12:58:32 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,32 +1,37 @@
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 
 module.exports = {
-  webpack(config, options) {
+  webpack(config, isServer) {
     config.output.publicPath = "auto";
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "hostApp",
-        filename: "static/chunks/remoteEntry.js",
-        remotes: {
-          remoteApp:
-            "remoteApp@https://aquamarine-rolypoly-b9d699.netlify.app/_next/static/chunks/remoteEntry.js", // Expose your SSR page
-        },
-
-        shared: {
-          react: {
-            singleton: true,
-
-            requiredVersion: false, // Use the version installed in the host app
+    if (isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "hostApp",
+          filename: "static/chunks/remoteEntry.js",
+          remotes: {
+            remoteApp:
+              "remoteApp@https://aquamarine-rolypoly-b9d699.netlify.app/_next/static/chunks/remoteEntry.js", // Expose your SSR page
           },
 
-          "react-dom": {
-            singleton: true,
+          shared: {
+            react: {
+              singleton: true,
 
-            requiredVersion: false, // Use the version installed in the host app
+              requiredVersion: false, // Use the version installed in the host app
+            },
+
+            "react-dom": {
+              singleton: true,
+
+              requiredVersion: false, // Use the version installed in the host app
+            },
           },
-        },
-      })
-    );
+          extraOptions: {
+            force: true,
+          },
+        })
+      );
+    }
 
     return config;
   },
